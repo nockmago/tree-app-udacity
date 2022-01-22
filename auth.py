085c09e15eb72616d7bfb97bbd989d7d56008ef9
nockmago <nicolamaglio8@gmail.com>
2022-01-22 10:18:35 +0100

    Update environment vars

--- auth.py
+++ auth.py
@@ -5,9 +5,9 @@ from jose import jwt
 from urllib.request import urlopen
 
 
-AUTH0_DOMAIN = 'tree-app.eu.auth0.com'
-ALGORITHMS = ['RS256']
-API_AUDIENCE = 'tree'
+AUTH0_DOMAIN = os.environ['AUTH0_DOMAIN']
+ALGORITHMS = os.environ['AUTH0_ALGORITHMS']
+API_AUDIENCE = os.environ['API_AUDIENCE']
 
 ## AuthError Exception
 '''
