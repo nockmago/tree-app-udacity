530d66743bc072151ab94febfca9d8c5467a1419
nockmago <nicolamaglio8@gmail.com>
2022-01-22 10:20:34 +0100

    Fix import mistake

--- auth.py
+++ auth.py
@@ -3,7 +3,7 @@ from flask import request, _request_ctx_stack, abort
 from functools import wraps
 from jose import jwt
 from urllib.request import urlopen
-
+import os
 
 AUTH0_DOMAIN = os.environ['AUTH0_DOMAIN']
 ALGORITHMS = os.environ['AUTH0_ALGORITHMS']
